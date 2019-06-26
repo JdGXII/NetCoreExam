f6f6d01c9ac594adb8d92ab5da568224ec20d805
Jose Alzaibar <jralzaibar@yahoo.com>
2019-06-26 01:15:08 -0500

    Convenciones mas filtro basico (sin automapeo aun)

---
+++ ExamenNetCore/ApiConventions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+
+namespace ExamenNetCoreApi
+{
+    public static class ApiConventions
+    {
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Get([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)][ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Get()
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Post([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)][ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Put([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)][ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Delete([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)][ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+    }
+}
---
+++ ExamenNetCore/Filters/PlaylistResultFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace ExamenNetCoreApi.Filters
+{
+    public class PlaylistResultFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var resultFromAction = context.Result as ObjectResult;
+            if (resultFromAction?.Value == null
+                || resultFromAction.StatusCode < 200
+                || resultFromAction.StatusCode < 200)
+            {
+                await next();
+                return;
+            }           
+
+            await next();
+        }
+    }
+}
